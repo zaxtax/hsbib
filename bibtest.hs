f27288750f53591c8e7eaeff98786290ca92b908
Javier Rosa <javirosa@oldflayn.(none)>
2008-11-29 12:06:57 -0500

    added some basic queries to bibTest.hs

--- bibtest.hs
+++ bibtest.hs
@@ -4,7 +4,9 @@ import Text.ParserCombinators.Parsec
 
 import BibParse
 import BibPrint
-
+import Data.List
+import Maybe
+import Char
 main = do
   test
   test2
@@ -27,4 +29,24 @@ testPretty = do
     Right table -> putStr (foldr (++) "" (map entryToStr table))
 
 
+type EntryKey = String
+type FieldKey = String
+type Table    = [Entry]
+findEntry :: Table->EntryKey->Maybe Entry
+findEntry t ek = find (\(ek',_) -> ek' == (map toLower ek) ) t 
 
+lookupKeyValue_ :: Entry->Key->Maybe Value
+lookupKeyValue_ (n,fs) k = case (find (\(k',_) -> k' == (map toLower k) ) fs) of
+                             Just kv -> Just (snd kv)
+                             Nothing -> Nothing
+
+lookupKeyValue :: Table->EntryKey->Key-> Maybe Value
+lookupKeyValue t ek k = case (find ( \(ek',fs) -> ek' == (map toLower ek) ) t ) of 
+                          Just e -> lookupKeyValue_ e k
+                          Nothing -> Nothing
+getWithTag :: Table->String->Table
+getWithTag t s = filter (\e -> hasTag e s) t 
+hasTag :: Entry->String->Bool
+hasTag e s = case lookupKeyValue_ e "tag" of
+               Nothing -> False
+               Just s' -> isInfixOf (map toLower s ) (map toLower s')
