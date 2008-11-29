ce14e4e0735b718380bf4da2fd196297f02cd74a
cf <cf@dynamic-134-098.usc.edu>
2008-11-29 09:32:25 -0800

    Refactored again

---
+++ bibutils.hs
@@ -0,0 +1,28 @@
+module BibUtils where
+
+import Data.List
+import Data.Maybe
+import Data.Char
+import BibParse
+
+type EntryKey = String
+type FieldKey = String
+type Table    = [Entry]
+findEntry :: Table->EntryKey->Maybe Entry
+findEntry t ek = find (\(ek',_) -> ek' == (map toLower ek) ) t 
+
+lookupKeyValue_ :: Entry->Key->Maybe Value
+lookupKeyValue_ (n,fs) k = case (find (\(k',_) -> k' == (map toLower k) ) fs) of
+                             Just kv -> Just (snd kv)
+                             Nothing -> Nothing
+
+lookupKeyValue :: Table->EntryKey->Key-> Maybe Value
+lookupKeyValue t ek k = case (find ( \(ek',fs) -> ek' == (map toLower ek) ) t ) of 
+                          Just e -> lookupKeyValue_ e k
+                          Nothing -> Nothing
+getWithTag :: Table->String->Table
+getWithTag t s = filter (\e -> hasTag e s) t 
+hasTag :: Entry->String->Bool
+hasTag e s = case lookupKeyValue_ e "tag" of
+               Nothing -> False
+               Just s' -> isInfixOf (map toLower s ) (map toLower s')
