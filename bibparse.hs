f53862d2a05a8626aa2e10e2c18fc6eccd292079
Javier Rosa <javirosa@oldflayn.(none)>
2008-11-29 06:58:59 -0500

    JR Added basic prettyPrinter code

--- bibparse.hs
+++ bibparse.hs
@@ -3,6 +3,7 @@ module Text.Parser.Bibtex where
 import Text.ParserCombinators.Parsec
 import Text.Parsec.Token
 import Text.Parsec.Language (haskellDef)
+import List
 
 run :: Show a => Parser a -> String -> IO ()
 run p input
@@ -61,4 +62,30 @@ entry = do
 
 bibfile = do {white; sepBy1 entry white}
  
-test = parseFromFile bibfile "/home/cf/papers/papers.bib"
+test =  (parseFromFile bibfile "/home/javirosa/papers/papers.bib")
+
+testPretty = do 
+  out <- test
+  case out of
+    Left err -> print err
+    Right table -> putStr (foldr (++) "" (map entryToStr table))
+
+type Key = String
+type Value = String
+type Field  = (Key,Value)
+type Entry = (String,[Field])
+
+keyToStr   :: String->String
+valueToStr :: String->String
+fieldToStr :: (String,String)->String
+entryToStr :: (String,[(String,String)])->String
+
+keyToStr   s        = s
+valueToStr v        = "{" ++ (v ++"}")
+fieldToStr (k,v)    = "  " ++ k ++ " = " ++ (valueToStr v)
+entryToStr (name,((_,doctype):f))    
+           = let 
+                 fStr = foldl1 conc (map fieldToStr f)
+                   where conc x y = x ++ ",\n" ++ y
+             in  "@" ++ doctype ++ " { " ++ name ++ ",\n" ++ fStr ++ "\n}"
+--Should have a more idealistic way of getting the doctype
