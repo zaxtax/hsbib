2ef41385ff9cf1f0841da37d662315a51e3c3f83
cf <cf@dynamic-153-063.usc.edu>
2008-12-03 10:23:23 -0800

    Minor bugfixes

--- bibconsole.hs
+++ bibconsole.hs
@@ -41,7 +41,7 @@ commands = [("help", "   -- show help", complete_none)
            ,("quit", "   -- quit",  complete_none)]
 
 docOpen docs e = do
-  mapM (\ x-> runCommand ("gv " ++ x)) 
+  mapM (\ x-> runCommand ("gv \"" ++ x ++ "\"")) 
        (catMaybes $ map (lookupKeyValue e "src") docs)
     
                  
@@ -141,11 +141,11 @@ repl prev db = do
 
 execute :: [String] -> Maybe [String] -> [Entry] -> IO (Maybe [String],[Entry])
 execute ["help"] _ e = putStr (usageInfo commands) >> return (Just [],e)
-execute ["open"] r e = docOpen (fromJust r) e >> return (Just [],e) -- should just pass all the last values found
-execute ("open":xs) _ e = docOpen xs e >> return (Just [],e)
+execute ["open"] r e = docOpen (fromJust r) e >> return (r,e) -- should just pass all the last values found
+execute ("open":xs) r e = docOpen xs e >> return (r,e)
 execute ("find":xs) _ e = mapM_ (putStrLn . printEntry) res >> return (Just (head $ transpose res),e) where res = search xs e
 execute ("print":xs) _ e = putStrLn (concatMap (show . findEntry e) xs) >> return (Just [],e) -- to debug
 execute ["quit"] _ e = return (Nothing,e)
-execute ["version"] _ e = putStrLn "hsbib: 0.1" >> return (Just [],e)
+execute ["version"] r e = putStrLn "hsbib: 0.1" >> return (Just [],e)
 execute ["list"] _ e = putStrLn (show e) >> return (Just [],e) -- to debug
-execute debug _ e = putStr (concatMap id $ ":":debug++[":\n"]) >> return (Just [],e)
+execute debug r e = putStr (concatMap id $ ":":debug++[":\n"]) >> return (Just [],e)
