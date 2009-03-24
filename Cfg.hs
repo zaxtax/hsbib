322c1b43e11c0266bf85ded88af72f83bbf48342
cf <cf@dynamic-162-106.usc.edu>
2009-03-24 02:40:58 -0700

    Cfg file crufty but working
    Merge into Console.hs now doable

--- Cfg.hs
+++ Cfg.hs
@@ -1,73 +1,45 @@
-{-# OPTIONS_GHC -XPatternGuards -XDeriveDataTypeable #-} 
 module Cfg where
 
 import System.Directory
 import System.FilePath
 import System.FilePath
 
-import Data.Generics
-
+import Data.Maybe
 import Data.List
 import Data.Either.Utils
 import Language.Haskell.Interpreter
 
-data Cfg = Cfg {files :: [String], views :: [(String,String)]} deriving (Typeable, Show)
+data Cfg = Cfg {files :: [String], views :: [(String,String)]} deriving (Show)
 
-defaultCfg = Cfg {files = [],views=[(".pdf","gv"),(".ps","gv")]}
+defaultCfg = Cfg {files = [],views=[(".pdf","gv"),(".ps","gv")]} -- (1)
 parsedCfg  = defaultCfg {files = ["theory.bib"]}
 
-unFun (Fun x) = x
+mkVal f n setFields = interpret (fromMaybe (show $ f defaultCfg) $ lookup n setFields)
 
 --ToDo:
---fallback to home directory ".bibconsole"
-
-
-{- 
-
-loadCfg defaultCfg {files,defaultviewer,views}
-
--}
-
--- loadDefaultCFGFile = do
---    cdir <- getCurrentDirectory 
---    cfgFile  <- readfile cp (cdir </> defaultCFGFileName) 
---    return $ forceEither cfgFile
-
--- getBibFilePaths :: ConfigParser -> [String]
--- getBibFilePaths cp = map snd $ forceEither $ items cp "bibfiles"
-
--- getDocumentAssociations :: ConfigParser -> [(String,String)]
--- getDocumentAssociations cp = 
---    zip extensions viewers
---    where extensions = filter ((/= '\'').head) $ forceEither $ options cp "file_assoc" --exludes the substitute vars which start with '
---          viewers    =  map (forceEither . get cp "file_assoc")  extensions
-
-
-lookupDocumentViewer :: [(String,String)]->FilePath->Maybe String
-lookupDocumentViewer das fp = 
-    case lookup fext das of
-      Just a  -> Just a
-      Nothing -> lookup "defaultviewer" das
-    where fext = takeExtension fp
-
+-- fallback to home directory ".bibconsole"
+-- refactor so adding Cfg fields is easier
+--   now must add
 
 getCfg :: Interpreter Cfg
 getCfg = do
   loadModules ["/home/cf/.hsbib/config.hs"]
   exports <- getModuleExports "UserConfig"
-  setTopLevelModules ["UserConfig","Cfg"]
+  setTopLevelModules ["UserConfig"]
   setImportsQ [("Prelude", Nothing)]
-  let setKeys = filter (flip elem ["files","views"]) (map unFun exports)
+  let setKeys = map name exports
   setVals <- mapM eval setKeys
-  let setFields = zipWith (\ k v -> k++"="++v) setKeys setVals
-  let expr =  "defaultCfg {" ++ (intercalate "," setFields) ++ "}"
-  interpret expr (as :: Cfg)
+  let setFields = zip setKeys setVals
+  
+  fileVal <- mkVal files "files" setFields (as :: [String]) -- (2)
+  viewVal <- mkVal views "views" setFields (as :: [(String,String)]) -- (3)
+  return Cfg {files=fileVal,views=viewVal} -- (4)
 
 main :: IO ()
 main = do r <- runInterpreter getCfg
           case r of
-            Left err -> putStrLn $ "Ups... " ++ (show err)
-            Right _ -> putStrLn "that's all folks"
+            Left err -> putStrLn $ "Ups... " ++ show err
+            Right cfg -> print cfg
 
 say :: String -> Interpreter ()
 say = liftIO . putStrLn
